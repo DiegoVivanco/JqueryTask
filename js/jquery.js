0cefd8c4b91a83b419d2822302b86c066200ff54
Diego Leonardo Vivanco Moreira <leox_vm@hotmail.com>
2017-04-22 21:43:03 +0200

    jqueryTask

---
+++ js/jquery.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dlvivanco on 22/04/2017.
+ */
+$( function() {
+    $( "#vigilanteUno" ).draggable();
+} );
+
+$( function() {
+    $( "#vigilanteDos" ).draggable();
+} );
+
+$( function() {
+    $( "#supervivor" ).draggable();
+} );
+
+$( function() {
+    $( "#puntoDeVigilancia" ).draggable();
+} );
+
+$( function() {
+    $( "#puntoDeSupervision" ).draggable();
+} );
